a4ecf19f6d599ea5561a1af8264d53bca7235682
Piyush220803 <158078484+Piyush220803@users.noreply.github.com>
2024-09-09 22:09:43 +0530

    script.js

--- Small Projects (Full Stack)/Weather App/Notes App/script.js
+++ Small Projects (Full Stack)/Weather App/Notes App/script.js
@@ -3,29 +3,29 @@ const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
 function showNotes() {
-    notesContainer.innerHTML = localStorage.getItem("notes");
+    notesContainer.innerHTML = localStorage.getItem("notes");   // Use to show written notes after refreshing or again opening the app
 }
 showNotes();
 
 function updateStorage() {
-    localStorage.setItem("notes", notesContainer.innerHTML);
+    localStorage.setItem("notes", notesContainer.innerHTML);   // use to store the written notes in the local storage 
 }
 
 createBtn.addEventListener("click", ()=>{
     let inputBox = document.createElement("p");
-    let img = document.createElement("img");
+    let img = document.createElement("img");                  // the delete button
     inputBox.className = "input-box";
-    inputBox.setAttribute("contenteditable", "true");
-    img.src = "images/delete.png";
-    notesContainer.appendChild(inputBox).appendChild(img);
+    inputBox.setAttribute("contenteditable", "true");         // to make the paragraph editable
+    img.src = "images/delete.png";                            // image of delete icon
+    notesContainer.appendChild(inputBox).appendChild(img);    // means add both of them to notesContainer
 })
 
 notesContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "IMG"){
-        e.target.parentElement.remove();
-        updateStorage();
+    if(e.target.tagName === "IMG"){                            // if the user click on the img button or delete icon,
+        e.target.parentElement.remove();                        // the current written notes will be removed and the update Storage 
+        updateStorage();                                           // will update the local storage that the written notes is removed
     }
-    else if(e.target.tagName === "P"){
+    else if(e.target.tagName === "P"){                         // here the written notes will be added to notes section and update by local storage
         notes = document.querySelectorAll(".input-box");
         notes.forEach(nt => {
             nt.onkeyup = function(){
@@ -35,7 +35,7 @@ notesContainer.addEventListener("click", function(e){
     }
 })
 
-document.addEventListener("keydown", event =>{
+document.addEventListener("keydown", event =>{                    // use to get to the next line when press ENTER
     if(event.key === "Enter"){
         document.execCommand("insertLineBreak");
         event.preventDefault();
